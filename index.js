b82c82b47f8032e21f2eed6c204b5cfa02f61620
IshiToyo <sansekidragonil@gmail.com>
2017-03-29 03:38:54 +0200

    Brunito

---
+++ index.js
@@ -0,0 +1,19 @@
+var express =require('express'),
+    app = express(),
+    http = require('http'),
+    httpServer = http.Server(app);
+    port = 3000;
+
+var Git = require("nodegit");
+
+
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.listen(port, function () {
+    console.log('App listen in Port: ' + port);
+});
